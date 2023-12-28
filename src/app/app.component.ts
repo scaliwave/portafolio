9595de0656435273bc9acc0d78133134df6c0978
Thms <thomasog10@gmail.com>
2023-12-27 21:59:37 -0500

    main-content-create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
